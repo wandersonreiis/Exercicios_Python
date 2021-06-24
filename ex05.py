145da513df7a8c0b0e506f671a8759303c308f31
wandersonreiis <wwmaggot@gmail.com>
2021-06-23 23:01:35 -0300

    Exercicios em Python atualizando ex04 a ex06

--- ex05.py
+++
@@ -1,6 +0,0 @@
-#Faça um Programa que converta metros para centímetros.
-metros = int(input("Digite quantos metros deseja converter: "))
-
-centimetros = metros * 100
-
-print(metros, "Metros = ", centimetros, "Centimetros")
