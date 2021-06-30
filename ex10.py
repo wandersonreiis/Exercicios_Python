23d2e0fd76045a679c7876851b7be1f8a3a260a8
wandersonreiis <wwmaggot@gmail.com>
2021-06-29 21:37:53 -0300

    Removendo duplicidades

--- ex10.py
+++
@@ -1,6 +0,0 @@
-#10. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
-tempcel= float(input('Digite a temperatura em Fgraus celsios '))
-tempfahrenheit= (tempcel * 9/5) + 32
-
-
-print('A temperatura atual é: ', tempfahrenheit,'°F')
