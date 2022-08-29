039a1a414fa4576b8105573bd5ccdab438be9b42
wandersonreiis <wwmaggot@gmail.com>
2022-08-28 22:16:01 -0300

    Atualização do Ex. 14

--- Estrutura_de_decisao/ex14.py
+++ Estrutura_de_decisao/ex14.py
@@ -5,4 +5,34 @@
 #   Entre 6.0 e 7.5         C
 #   Entre 4.0 e 6.0         D
 #   Entre 4.0 e zero        E
-# O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
+
+
+
+n1 = float(input('Digite uma nota: '))
+n2 = float(input('Digite outra nota: '))
+while n1 >10 or n2>10 :
+    print('O Valor Max da nota é 10:')
+    n1 = float(input('Digite uma nota: '))
+    n2 = float(input('Digite outra nota: '))
+
+media = (n1+n2) / 2
+
+if media < 4:
+    conceito = 'E'
+    print('Sua média é: ', media, ' e seu conceito é : ', conceito)
+
+elif media >=4 and media <6 :
+    conceito = 'D'
+    print('Sua média é: ', media, ' e seu conceito é : ', conceito)
+
+elif media >=6 and media <7.5 :
+    conceito = 'C'
+    print('Sua média é: ', media, ' e seu conceito é : ', conceito)
+
+elif media >=7.5 and media <9 :
+    conceito = 'B'
+    print('Sua média é: ', media, ' e seu conceito é : ', conceito)
+
+else:
+    conceito = 'A'
+    print('Sua média é: ', media, ' e seu conceito é : ', conceito)
